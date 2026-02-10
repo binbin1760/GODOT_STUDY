2bb4eaee2ef8b59493c599c0172d15a52971bc78
binbin1760 <569279172@qq.com>
2026-02-11 00:27:35 +0800

    add  jump_buffer_time  in second_fail file

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ## 该仓库所有东西都有可能存在版权问题
 ## 该仓库所有东西均只供学习使用
+
+## items to be Optimized
+	1. i should add climb_states,which would slove the problem not having coyote_time
--- player/states/second_fail.gd
+++ player/states/second_fail.gd
@@ -5,7 +5,10 @@ class_name  player_state_second_fail extends player_state
 	#2.当玩家进入下落状态时，只要在缓冲时间内均可以跳跃
 	#代码流程： coyote_time =0.125 =>当用户按跳跃键时检测两点第一 前一个状态是否为跳跃状态 第二：是否在coyote_time时间内
 @export var second_fail_gravity = 980.0*2
-@export var buffer_time:float = 0.1
+@export var buffer_time:float = 0.0125
+
+var buffer_timer:float = 0
+
 func init()->void:
 	pass
 
@@ -22,9 +25,13 @@ func exit()->void:
 
 
 func handle_inupt(event:InputEvent)->player_state:
+	if Input.is_action_pressed("move_jump") and player.previous_state == second_jump:
+		buffer_timer = buffer_time
 	return next_state
 
 func  process(_delta:float)->player_state:
+	#跳跃优化_设置跳跃缓冲_更新缓冲时间
+	buffer_timer  -=_delta
 	if Input.is_action_pressed("move_left") and player.velocity.x >0:
 		player.velocity.x = player.velocity.x*-1
 	if Input.is_action_pressed("move_right") and player.velocity.x < 0:
@@ -33,13 +40,13 @@ func  process(_delta:float)->player_state:
 	if not player.is_on_floor():
 		var new_direct = Input.get_axis("move_left","move_right")
 		player.velocity.x = %run.run_in_hair * new_direct
-	if player.is_on_floor() and player.velocity.x == 0:
+	if player.is_on_floor() and player.velocity.x == 0 and buffer_timer <=0:
 		return idle	
-	if player.is_on_floor() and player.velocity.x !=0:
+	if player.is_on_floor() and player.velocity.x !=0 and buffer_timer <=0:
 		return run	
-#	#跳跃优化_设置跳跃缓冲
+	if player.is_on_floor() and buffer_timer >0:
+		return jump	
 	return next_state
 
 func physics_process(_delta:float)->player_state:
-	
 	return next_state
